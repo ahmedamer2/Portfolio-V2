3225aacbed6f96f897feef0767ab5b70869dcdca
Ahmed's Mackbook <ahmedsay01@gmail.com>
2022-09-23 19:59:06 -0600

    Finished hero and Header

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,21 +3,29 @@ import Head from "next/head";
 import Image from "next/image";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import About from "../components/About";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
    return (
-      <div>
+      <div className="bg-[#EAE7DC] text-[#8E8D8A] h-screen snap-y snap-mandatory overflow-scroll z-0 scroll-smooth">
          <Head>
             <title>Ahmed Amer</title>
          </Head>
 
          {/* Header */}
          <Header />
+
          {/* Hero  */}
-         <section id="hero">
+         <section id="hero" className="snap-start">
             <Hero />
          </section>
+
+         {/* About */}
+         <section id="about" className="snap-center">
+            <About />
+         </section>
+
          {/* Experiences */}
 
          {/* Skills */}
