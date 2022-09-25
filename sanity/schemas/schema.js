631e0cc60137b984f62780c1f7dcbdf0b7e91097
Ahmed's Mackbook <ahmedsay01@gmail.com>
2022-09-25 01:55:30 -0600

    create Sanity schemas

--- sanity/schemas/schema.js
+++ sanity/schemas/schema.js
@@ -5,9 +5,17 @@ import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 import pageInfo from './pageInfo'
+import skill from "./skill"
+import project from "./project"
+import social from './social'
+import experience from './experience'
 export default createSchema({
   name: 'default',
   types: schemaTypes.concat([
     pageInfo,
+    skill,
+    project,
+    social,
+    experience
   ]),
 })
